7948ec4edcf08a6f9911c35bf1f075dbdc584404
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-14 14:38:35 +0300

    Initials

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import PlantPage from './components/PlantPage'
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+     <PlantPage />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header=()=>{
+    return (
+        <div>
+            <h1>
+                Plants
+                <span className="logo" role="img">
+                🌱
+                </span>
+            </h1>
+        </div>
+    )
+}
+export default Header
